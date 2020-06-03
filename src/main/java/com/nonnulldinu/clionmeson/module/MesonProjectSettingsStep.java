a282785f9b2db5576aaed0f29bc5c92b06ada5f1
Jon <jdefilla@gmail.com>
2020-06-03 20:58:59 +0200

    Fixed warnings with the comboboxes

--- src/main/java/com/nonnulldinu/clionmeson/module/MesonProjectSettingsStep.java
+++ src/main/java/com/nonnulldinu/clionmeson/module/MesonProjectSettingsStep.java
@@ -25,7 +25,7 @@ public class MesonProjectSettingsStep extends ProjectSettingsStepBase<Ref<String
 
 		// language label and combobox
 		final JLabel languageLabel = new JLabel("Language:");
-		ComboBox languageCombobox = new ComboBox<>(new String[] {"C", "C++", "Rust", "Java"});
+		ComboBox<String> languageCombobox = new ComboBox<>(new String[] {"C", "C++", "Rust", "Java"});
 		languageCombobox.setSelectedItem("C++");
 
 		jPanel.add(languageLabel, new GridBagConstraints(0, 0, 1, 1, 0, 0, GridBagConstraints.LINE_START, 0, JBUI.emptyInsets(), 0, 0));
@@ -33,7 +33,7 @@ public class MesonProjectSettingsStep extends ProjectSettingsStepBase<Ref<String
 
 		// standard label and combobox
 		final JLabel standardLabel = new JLabel("Language standard:");
-		ComboBox standardCombobox = new ComboBox<>();
+		ComboBox<String> standardCombobox = new ComboBox<>();
 
 		jPanel.add(standardLabel, new GridBagConstraints(0, 1, 1, 1, 0, 0, GridBagConstraints.LINE_START, 0, JBUI.emptyInsets(), 0, 0));
 		jPanel.add(standardCombobox, new GridBagConstraints(1, 1, 1, 1, 0, 0, GridBagConstraints.LINE_START, 0, JBUI.emptyInsets(), 0, 0));
@@ -42,28 +42,27 @@ public class MesonProjectSettingsStep extends ProjectSettingsStepBase<Ref<String
 		jPanel.add(Box.createGlue(), new GridBagConstraints(2, 2, 1, 2, 1, 1, GridBagConstraints.CENTER, 0, JBUI.emptyInsets(), 0, 0));
 
 		// listeners
-		languageCombobox.addActionListener (new ActionListener() { // see: https://stackoverflow.com/questions/58939/jcombobox-selection-change-listener
-			public void actionPerformed(ActionEvent e) {
-				DefaultComboBoxModel model;
-				switch(languageCombobox.getSelectedIndex()) {
-					case 0:
-						model = new DefaultComboBoxModel(new String[] {"C90", "C99", "C11"});
-						break;
-					case 1:
-						model = new DefaultComboBoxModel(new String[] {"C++98", "C++11", "C++14", "C++17", "C++20"});
-						break;
-					case 2:
-						model = new DefaultComboBoxModel(new String[] {"Rust v1.38.0"});
-						break;
-					case 3:
-						model = new DefaultComboBoxModel(new String[] {"Java 8", "Java 11", "Java 14"});
-						break;
-					default:
-						model = new DefaultComboBoxModel();
-				}
-				standardCombobox.setModel(model);
-				standardCombobox.setSelectedIndex(standardCombobox.getModel().getSize() - 1);
+		// see: https://stackoverflow.com/questions/58939/jcombobox-selection-change-listener
+		languageCombobox.addActionListener (e -> {
+			DefaultComboBoxModel<String> model;
+			switch(languageCombobox.getSelectedIndex()) {
+				case 0:
+					model = new DefaultComboBoxModel<>(new String[] {"C90", "C99", "C11"});
+					break;
+				case 1:
+					model = new DefaultComboBoxModel<>(new String[] {"C++98", "C++11", "C++14", "C++17", "C++20"});
+					break;
+				case 2:
+					model = new DefaultComboBoxModel<>(new String[] {"Rust v1.38.0"});
+					break;
+				case 3:
+					model = new DefaultComboBoxModel<>(new String[] {"Java 8", "Java 11", "Java 14"});
+					break;
+				default:
+					model = new DefaultComboBoxModel<>();
 			}
+			standardCombobox.setModel(model);
+			standardCombobox.setSelectedIndex(standardCombobox.getModel().getSize() - 1);
 		});
 
 		// emit the signal update the standardCombobox and select the last standard
