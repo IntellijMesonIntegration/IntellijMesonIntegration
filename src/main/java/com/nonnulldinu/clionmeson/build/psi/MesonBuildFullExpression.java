278c95cda73fd25c5f60a09d328ac670bea05419
Dinu Blanovschi <dinu.blanovschi001@gmail.com>
2020-06-03 16:33:55 +0300

    Forgot to add to git again :'(

---
+++ src/main/java/com/nonnulldinu/clionmeson/build/psi/MesonBuildFullExpression.java
@@ -0,0 +1,16 @@
+// This is a generated file. Not intended for manual editing.
+package com.nonnulldinu.clionmeson.build.psi;
+
+import java.util.List;
+import org.jetbrains.annotations.*;
+import com.intellij.psi.PsiElement;
+
+public interface MesonBuildFullExpression extends PsiElement {
+
+  @Nullable
+  MesonBuildExpression getExpression();
+
+  @Nullable
+  MesonBuildFullBoolExpression getFullBoolExpression();
+
+}
---
+++ src/main/java/com/nonnulldinu/clionmeson/build/psi/MesonBuildRelCheckOp.java
@@ -0,0 +1,13 @@
+// This is a generated file. Not intended for manual editing.
+package com.nonnulldinu.clionmeson.build.psi;
+
+import java.util.List;
+import org.jetbrains.annotations.*;
+import com.intellij.psi.PsiElement;
+
+public interface MesonBuildRelCheckOp extends PsiElement {
+
+  @Nullable
+  MesonBuildEqualityOp getEqualityOp();
+
+}
