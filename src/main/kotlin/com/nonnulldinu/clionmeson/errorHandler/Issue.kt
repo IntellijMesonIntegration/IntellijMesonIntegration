d5ddb13384e2b9b68edaba94c1ca22c396d62165
Dinu Blanovschi <dinu.blanovschi001@gmail.com>
2020-06-11 16:34:03 +0300

    Add labels to issue

--- src/main/kotlin/com/nonnulldinu/clionmeson/errorHandler/Issue.kt
+++ src/main/kotlin/com/nonnulldinu/clionmeson/errorHandler/Issue.kt
@@ -6,4 +6,24 @@ import com.google.gson.annotations.SerializedName
 /**
  * An issue that we can send to the bot to report
  */
-data class Issue(@SerializedName("issue-title") val title: String, @SerializedName("issue-body") val body: String, @SerializedName("issue-label") val label: String)
+data class Issue(@SerializedName("issue-title") val title: String, @SerializedName("issue-body") val body: String, @SerializedName("issue-labels") val label: Array<String>) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Issue
+
+        if (title != other.title) return false
+        if (body != other.body) return false
+        if (!label.contentEquals(other.label)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = title.hashCode()
+        result = 31 * result + body.hashCode()
+        result = 31 * result + label.contentHashCode()
+        return result
+    }
+}
